3ce169d98f1bc225f99a64b519c568889f779b93
happyalpacaa <wongjinwei2204@gmail.com>
2021-12-31 15:35:04 -0800

    added different routes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,70 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <nav>
+        <ul id="navigation">
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
+        <Switch>
+            <Route exact path="/">
+            <Home />
+            </Route>
+            <Route path="/about">
+            <About />
+            </Route>
+        </Switch>
+      </nav>
+  </div>
   );
+}
+
+export default App;
+
+import './App.css';
+import React from 'react';
+import { Route, Switch, Link } from "react-router-dom";
+import About from './About';
+import Home from './Home';
+
+class App extends React.Component {
+  render() {
+      return (
+        <div className="App">
+          <div>
+            <nav>
+              <ul id="navigation">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/about">About</Link>
+                </li>
+                <li>
+                <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+            <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+          </div>
+            );
+  }
 }
 
 export default App;
