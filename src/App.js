93b1ba20a446f09862d4ea08074354fcf82628c0
happyalpacaa <wongjinwei2204@gmail.com>
2021-12-31 16:12:11 -0800

    import { HashRouter } from 'react-router-dom'; in index.js

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ class App extends React.Component {
                   <Link to="/">Home</Link>
                 </li>
                 <li>
-                <Link to="/about">About</Link>
+                  <Link to="/about">About</Link>
                 </li>
                 <li>
-                <Link to="/contact">Contact</Link>
+                  <Link to="/contact">Contact</Link>
                 </li>
               </ul>
             </nav>
